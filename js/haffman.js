bee9a94d4b1752ff4b8acad8b6b3c427bb343947
Kevin Kong <fhxw@vip.qq.com>
2020-12-17 19:55:29 +0800

    完成了该项目

---
+++ js/haffman.js
@@ -0,0 +1,7 @@
+function Haffman(char,weight,lchild=null,rchild=null,code=0){
+    this.char = char;//字符
+    this.weight = weight;//权重
+    this.lchild = lchild;//左孩子
+    this.rchild = rchild;//右孩子
+    this.code = code;//编码
+}
